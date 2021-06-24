83847823a750d07127ffa776dde63fbef8a3f9e2
WikyPassu <alan_passucci@hotmail.com>
2021-06-24 19:32:38 -0300

    Conexion api

--- app/src/main/java/com/example/splabov/HiloConexion.java
+++ app/src/main/java/com/example/splabov/HiloConexion.java
@@ -4,23 +4,14 @@ import android.os.Handler;
 import android.os.Message;
 
 import org.json.JSONArray;
-import org.json.JSONException;
-import org.json.JSONObject;
-
-import java.util.ArrayList;
-import java.util.List;
 
 public class HiloConexion extends Thread {
 
-    public static final int IMAGEN = 1;
-    public static final int DATA = 2;
     Handler handler;
-    Boolean img;
     String url;
 
-    public HiloConexion(Handler handler, boolean img, String url){
+    public HiloConexion(Handler handler, String url){
         this.handler = handler;
-        this.img = img;
         this.url = url;
     }
 
@@ -28,41 +19,14 @@ public class HiloConexion extends Thread {
     public void run() {
         try{
             ConexionHTTP conexionHTTP = new ConexionHTTP();
-            if(!img){
-                byte[] dataJson = conexionHTTP.obtenerRespuesta(this.url);
-                String s = new String(dataJson);
-                Message msg = new Message();
-                msg.arg1 = DATA;
-                msg.obj = this.parserJson(s);
-                this.handler.sendMessage(msg);
-
-            }
-            else{
-                byte[] img = conexionHTTP.obtenerRespuesta(this.url);
-                Message msg = new Message();
-                msg.arg1 = IMAGEN;
-                msg.obj = img;
-                this.handler.sendMessage(msg);
-            }
+            byte[] dataJson = conexionHTTP.obtenerRespuesta(this.url);
+            String s = new String(dataJson);
+            Message msg = new Message();
+            JSONArray jsonArray = new JSONArray(s);
+            msg.obj = jsonArray;
+            this.handler.sendMessage(msg);
         } catch(Exception e){
             e.printStackTrace();
         }
-    }
-
-    public List<String> parserJson(String s){
-        List<String> data = new ArrayList<>();
-        try{
-            if(!"".equals(s)){
-                JSONArray jsonArray = new JSONArray(s);
-                //Lo recorrro, extraigo lo que necesite y lo pongo en la lista
-                for(int i=0; i<jsonArray.length(); i++){
-                    JSONObject jsonObject = jsonArray.getJSONObject(i);
-                    data.add(jsonObject.getString("name"));
-                }
-            }
-        } catch (JSONException e) {
-            e.printStackTrace();
-        }
-        return data;
     }
 }
--- app/src/main/java/com/example/splabov/MainActivity.java
+++ app/src/main/java/com/example/splabov/MainActivity.java
@@ -4,6 +4,8 @@ import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.SearchView;
 
+import android.content.Context;
+import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.os.Handler;
 import android.os.Looper;
@@ -11,23 +13,42 @@ import android.os.Message;
 import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
 import android.widget.TextView;
 
-import java.util.List;
+import org.json.JSONArray;
+import org.json.JSONException;
 
 public class MainActivity extends AppCompatActivity implements Handler.Callback, SearchView.OnQueryTextListener {
 
     Handler handler;
-    TextView tvData;
+    TextView tvUsuarios;
+    SharedPreferences pref;
+    SharedPreferences.Editor editor;
+    View view;
+    JSONArray jsonUsuarios;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        this.handler = new Handler(Looper.myLooper(), this);
-        HiloConexion hiloApi = new HiloConexion(this.handler, false, "http://restcountries.eu/rest/v2/all");
-        hiloApi.start();
+        this.tvUsuarios = super.findViewById(R.id.tvUsuarios);
+        this.pref = getPreferences(Context.MODE_PRIVATE);
+        this.editor = this.pref.edit();
+        try {
+            if(!this.pref.contains("jsonArray")){
+                this.handler = new Handler(Looper.myLooper(), this);
+                HiloConexion hiloApi = new HiloConexion(this.handler,"http://10.0.2.2:3001/usuarios");
+                hiloApi.start();
+            }
+            else{
+                this.jsonUsuarios = new JSONArray(this.pref.getString("jsonArray", null));
+                this.tvUsuarios.setText(this.jsonUsuarios.toString());
+            }
+        } catch (JSONException e) {
+            e.printStackTrace();
+        }
     }
 
     @Override
@@ -63,18 +84,12 @@ public class MainActivity extends AppCompatActivity implements Handler.Callback,
 
     @Override
     public boolean handleMessage(@NonNull Message message) {
-        if(message.arg1 == HiloConexion.DATA){
-            this.tvData = super.findViewById(R.id.tvData);
-            List<String> data = (List<String>) message.obj;
-            tvData.setText(data.toString());
-            Log.d("Callback", "Llegaron datos!!!");
-        }
-//        else if(message.arg1 == HiloConexion.IMAGEN){
-//            ImageView iv = super.findViewById(R.id.img);
-//            byte[] img = (byte[]) message.obj;
-//            iv.setImageBitmap(BitmapFactory.decodeByteArray(img, 0, img.length));
-//            Log.d("Callback", "Llego imagen");
-//        }
+        this.tvUsuarios = super.findViewById(R.id.tvUsuarios);
+        this.jsonUsuarios = (JSONArray) message.obj;
+        this.editor.putString("jsonArray", this.jsonUsuarios.toString());
+        this.editor.commit();
+        tvUsuarios.setText(this.jsonUsuarios.toString());
+        Log.d("Callback", "Llegaron datos!!!");
         return false;
     }
 }
